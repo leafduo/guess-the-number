0b366cb849d239c16992b683a0a959210bb836d7
Zuyang Kou <leafduo@gmail.com>
2012-07-17 15:03:31 +0800

    Change range of random number to 1..100.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,6 @@ var uuid = require('node-uuid');
 
 exports.getToken = function(req, res){
     var token = uuid.v4();
-    var randomNumber = Math.floor(Math.random() * 100);
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
     res.send({token: token, randomNumber: randomNumber});
 };
